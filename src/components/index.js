9a6d94048655fe01d6675646fba9665f289de1eb
Victorien <bvictorien+github@gmail.com>
2023-01-13 16:03:35 +0100

    Remove AdsButton Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export * from "./Softkey/Softkey";
 export * from "./Map/Map";
 export * from "./Map/Markers";
-export * from "./Ads/Button";
 export * from "./Card/Card";
 export * from "./Onboarding/Onboarding";
 export * from "./Results/Results";
