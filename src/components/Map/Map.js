d98d979c13d62b32d291a7e228695c19c14722d3
Victorien <bvictorien+github@gmail.com>
2023-03-08 14:53:38 +0100

    Supporting min and max zoom

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,94 +7,91 @@ import { getBounds, getCenter } from "../../tools";
 import { ReactComponent as Target } from "./images/target.svg";
 import "./Leaflet.1.7.1.css";
 
-
 //export function Map({ center, children, zoom }) {
 export const Map = React.forwardRef((props, ref) => {
-	const { center = [0, 0], children, keyboard = true, to } = props;
-	/* SETUP REF */
-	// This ref is use to update map parameters */
+  const { center = [0, 0], children, keyboard = true, to } = props;
+  /* SETUP REF */
+  // This ref is use to update map parameters */
 
-	/* DEFINE THE BOUNDS */
-	let [bounds, setBounds] = useState([]);
-	let [params, setParams] = useState([]);
+  /* DEFINE THE BOUNDS */
+  let [params, setParams] = useState([]);
 
+  /* We will define the higest and lowest bound's lattitudes and longitudes. By definition, latitude go from Nord to South with a scale of 90 to -90 and longitude go from West to Est with a scale of -180 to 180. If `A` is the Nord-Ouest corner, A.lgt should be the lowest value and A.lat should be the highest value. If `B` is the South-East corner, B.lgt should be the higgest value and B.lat should be the lowest value. */
+  //N.B.: I could do it more simple https://stackoverflow.com/questions/27451152/fitbounds-of-markers-with-leaflet
 
-	/* We will define the higest and lowest bound's lattitudes and longitudes. By definition, latitude go from Nord to South with a scale of 90 to -90 and longitude go from West to Est with a scale of -180 to 180. If `A` is the Nord-Ouest corner, A.lgt should be the lowest value and A.lat should be the highest value. If `B` is the South-East corner, B.lgt should be the higgest value and B.lat should be the lowest value. */
-	//N.B.: I could do it more simple https://stackoverflow.com/questions/27451152/fitbounds-of-markers-with-leaflet
+  useEffect(() => {
+    if (children) {
+      //Figure Out which points we will focus during the Zoom in
+      const pointsZoomSnapping = children.filter((child) => {
+        return child.props.zoomSnapping;
+      });
 
-	useEffect(() => {
-		// A and B are respectively the Nord-Ouest and South-East corner.
-		const A = { lat: null, lgt: null };
-		const B = { lat: null, lgt: null };
+      const zoomMaxBounds = getBounds(pointsZoomSnapping, (child) => {
+        return child.props.position;
+      });
 
-		// We will compare coords children
+      const zoomMaxLeafletBounds = L.latLngBounds(
+        L.latLng(zoomMaxBounds.NE.lat, zoomMaxBounds.NE.lng),
+        L.latLng(zoomMaxBounds.SW.lat, zoomMaxBounds.SW.lng)
+      );
 
-	
-		if (children) {
-			const pointsZoomSnapping = children.filter((child)=>{
-				return child.props.zoomSnapping;
-			})
-	
-			console.log(pointsZoomSnapping);
+      const defaultBounds = getBounds(children, (child) => {
+        return child.props.position;
+      });
 
-			children.forEach((child) => {
-				
-				if (child && child.props.position) {
-					// Marker should have a position property where the firs value of the array is the latitude and the second value of the array is the longitutde.
-					let C = child.props.position[0]; //lat
-					let D = child.props.position[1]; //lgt
+      const defaultLeafletBounds = L.latLngBounds(
+        L.latLng(defaultBounds.NE.lat, defaultBounds.NE.lng),
+        L.latLng(defaultBounds.SW.lat, defaultBounds.SW.lng)
+      );
 
-					// A.lat should be the highest value
-					A.lat = A.lat ? (A.lat > C ? A.lat : C) : C;
-					// B.lat should be the lowest value
-					B.lat = B.lat ? (B.lat < C ? B.lat : C) : C;
-					// A.lgt should be the lowest value
-					A.lgt = A.lgt ? (A.lgt < D ? A.lgt : D) : D;
-					//B.lgt should be the higgest value
-					B.lgt = B.lgt ? (B.lgt > D ? B.lgt : D) : D;
-				}
-			});
-			//Create the Bounds Object
-			const bounds = L.latLngBounds(L.latLng(A.lat, A.lgt), L.latLng(B.lat, B.lgt));
-			setParams({
-				defaultCenter: bounds.getCenter(),
-				defaultBounds: bounds
-			})
-			setBounds(bounds);
-		} else {
-			setBounds(L.latLngBounds(L.latLng(30, -60), L.latLng(-30, 60)));
-		}
-	}, [children]);
+      //Set the Bounds as Leaflet object
 
-	const token = "pk.eyJ1IjoiYnZpY3RvcmllbiIsImEiOiJja3RycWNhYmMwNGQ2MnVtaTNnMGNwMTJwIn0.HirDPOdiEpC0myYa1x45RA";
+      setParams({
+        minCenter: defaultLeafletBounds.getCenter(),
+        minZoom: L.getBoundsZoom(defaultLeafletBounds),
+        defaultBounds: defaultLeafletBounds,
+        maxCenter: zoomMaxLeafletBounds.getCenter(),
+        maxZoom: L.getBoundsZoom(zoomMaxLeafletBounds),
+      });
+    } else {
+      setParams({
+        defaultBounds: L.latLngBounds(L.latLng(30, -60), L.latLng(-30, 60)),
+      });
+    }
+  }, [children]);
 
-	const tiles = "https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token="+ token;
+  const token =
+    "pk.eyJ1IjoiYnZpY3RvcmllbiIsImEiOiJja3RycWNhYmMwNGQ2MnVtaTNnMGNwMTJwIn0.HirDPOdiEpC0myYa1x45RA";
 
-	return (
-		<div
-			className={css.Container}
-			data-fullscreen={keyboard}
-			data-update={to ? true : false}
-		>
-			{to && (
-				<div className={css.Target}>
-					<Target />
-				</div>
-			)}
-			{center && (
-				<MapContainer className={css.Map} keyboardPanDelta={0}>
-					<TileLayer url={tiles} />
-					{children}
-					<Set bounds={bounds} ref={ref} to={to} keyboard={keyboard} />
-				</MapContainer>
-			)}
-			<div className={css.Softkeys}>
-				<div>Zoom out</div>
-				<div>Select</div>
-				<div>Zoom In</div>
-			</div>
-		</div>
-	);
+  const tiles =
+    "https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token=" +
+    token;
+
+  return (
+    <div
+      className={css.Container}
+      data-fullscreen={keyboard}
+      data-update={to ? true : false}
+    >
+      {to && (
+        <div className={css.Target}>
+          <Target />
+        </div>
+      )}
+      {center && (
+        <MapContainer className={css.Map} keyboardPanDelta={0}>
+          <TileLayer url={tiles} />
+          {children}
+          <Set ref={ref} to={to} keyboard={keyboard} {...params} />
+        </MapContainer>
+      )}
+      <div className={css.Softkeys}>
+        <div>Zoom out</div>
+        <div>Select</div>
+        <div>Zoom In</div>
+      </div>
+    </div>
+  );
 }); /**/
 
 /* TRIGGER MAP UPDATE */
@@ -102,77 +99,84 @@ export const Map = React.forwardRef((props, ref) => {
 /* MapContainer props are immutable: changing them after they have been set a first time will have no effect on the Map instance or its container. The Leaflet Map instance created by the MapContainer element can be accessed by child components using one of the provided hooks or the MapConsumer component. The component is use to triger re-render on the map */
 
 const Set = React.forwardRef((props, ref) => {
-	const { bounds, to, keyboard } = props;
-	const map = useMap();
+  const {
+    to,
+    keyboard,
+    defaultBounds,
+    minCenter,
+    minZoom,
+    maxCenter,
+    maxZoom,
+  } = props;
+  const map = useMap();
 
-	if (ref) {
-		ref.current = map;
-	}
+  if (ref) {
+    ref.current = map;
+  }
 
-	/* Here, we setup all keyboard actions*/
-	//First, we define the incremental int from the keyboard
-	const i = 25;
-	//Then, we define the onKeyDown function
-	const onKeyDown = (evt) => {
-		if (evt.key === "1") {
-			map.zoomOut();
-		} else if (evt.key === "3") {
-			map.zoomIn();
-		}
-		if (!keyboard) return false;
-		//Events related to zoom
-		if (evt.key === "SoftLeft") {
-			map.zoomOut();
-		} else if (evt.key === "SoftRight") {
-			map.zoomIn();
-			//Events related to zoom
-		} else if (evt.key === "ArrowUp") {
-			map.panBy([0, -i], { animate: true });
-		} else if (evt.key === "ArrowDown") {
-			map.panBy([0, i], { animate: true });
-		} else if (evt.key === "ArrowLeft") {
-			map.panBy([-i, 0], { animate: true });
-		} else if (evt.key === "ArrowRight") {
-			map.panBy([i, 0], { animate: true });
-			//This last entry, trigger the onSubmit function and sent back the center of the map to the parent
-		} else if (evt.key === "Enter") {
-			//Go to display the results according to the coords
-			if (to) {
-				to({
-					coords: normalizeCoords(map.getCenter(), "map")
-				});
-			}
-		} else if (evt.key === "Backspace") {
-			if (to) {
-				to("Choose Location");
-			}
+  /* Here, we setup all keyboard actions*/
+  //First, we define the incremental int from the keyboard
+  const i = 25;
+  //Then, we define the onKeyDown function
+  const onKeyDown = (evt) => {
+    if (evt.key === "1") {
+      map.zoomOut();
+    } else if (evt.key === "3") {
+      map.zoomIn();
+    }
+    if (!keyboard) return false;
+    //Events related to zoom
+    if (evt.key === "SoftLeft") {
+      map.zoomOut();
+    } else if (evt.key === "SoftRight") {
+      map.zoomIn();
+      //Events related to zoom
+    } else if (evt.key === "ArrowUp") {
+      map.panBy([0, -i], { animate: true });
+    } else if (evt.key === "ArrowDown") {
+      map.panBy([0, i], { animate: true });
+    } else if (evt.key === "ArrowLeft") {
+      map.panBy([-i, 0], { animate: true });
+    } else if (evt.key === "ArrowRight") {
+      map.panBy([i, 0], { animate: true });
+      //This last entry, trigger the onSubmit function and sent back the center of the map to the parent
+    } else if (evt.key === "Enter") {
+      //Go to display the results according to the coords
+      if (to) {
+        to({
+          coords: normalizeCoords(map.getCenter(), "map"),
+        });
+      }
+    } else if (evt.key === "Backspace") {
+      if (to) {
+        to("Choose Location");
+      }
+    } else {
+      return;
+    }
+    evt.preventDefault();
+  };
+  //Secondly,  We mount the onKeyDown when the app start
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [keyboard]);
 
-		} else {
-			return;
-		}
-		evt.preventDefault();
-	};
-	//Secondly,  We mount the onKeyDown when the app start
-	useEffect(() => {
-		document.addEventListener("keydown", onKeyDown);
-		return () => document.removeEventListener("keydown", onKeyDown);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [keyboard]);
+  // We adjust the bounds of the map. We can use fitBounds for no animation and flyToBounds for animation.
+  useEffect(() => {
+    map.fitBounds(defaultBounds, {
+      // I don't undertsand anything about the value of the padding.
+      padding: [24, 48],
+    });
+  }, [defaultBounds, map]);
 
-	// We adjust the bounds of the map. We can use fitBounds for no animation and flyToBounds for animation.
-	useEffect(() => {
-		map.fitBounds(bounds, {
-			// I don't undertsand anything about the value of the padding.
-			padding: [24, 48]
-		});
-	}, [bounds, map]);
+  map.invalidateSize();
 
-	map.invalidateSize();
+  //Suppose to disa
+  //map.keyboard.disable();
 
-	//Suppose to disa
-	//map.keyboard.disable();
-
-	return null;
+  return null;
 });
 
 /* USEFULL LINKS, DATA AND API */
@@ -192,7 +196,7 @@ const Set = React.forwardRef((props, ref) => {
  
  */
 
- 	/* USEFULL LINKS 
+/* USEFULL LINKS 
 	var mapboxUrl =
 		"https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
 	var accessToken =
--- src/tools/geometry.js
+++ src/tools/geometry.js
@@ -1,84 +1,82 @@
 export function getDistance(lon1, lat1, lon2, lat2) {
-	var R = 6371; // Radius of the earth in km
-	var dLat = deg2rad(lat2 - lat1); // deg2rad below
-	var dLon = deg2rad(lon2 - lon1);
-	var a =
-		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-		Math.cos(deg2rad(lat1)) *
-			Math.cos(deg2rad(lat2)) *
-			Math.sin(dLon / 2) *
-			Math.sin(dLon / 2);
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	var d = R * c; // Distance in km
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
 
-	if (d < 1) {
-		return Math.round(d * 1000) + "m";
-	} else {
-		return Math.round(d) + "km";
-	}
+  if (d < 1) {
+    return Math.round(d * 1000) + "m";
+  } else {
+    return Math.round(d) + "km";
+  }
 }
 
 export function deg2rad(deg) {
-	return deg * (Math.PI / 180);
+  return deg * (Math.PI / 180);
 }
 
-
 // PATH BOUNDS
-export function getBounds(points) {
-	const NE = { lat: 0, lng: 0 }; // Nord East
-	const SW = { lat: 0, lng: 0 }; // South West
-  
-	points.forEach((item) => {
-	  const lat = item[0]; // latitude
-	  const lng = item[1]; // longitude
-  
-	  // NW.lat should be the highest value
-	  NE.lat = NE.lat ? (NE.lat > lat ? NE.lat : lat) : lat;
-	  // NW.lng should be the higgest value
-	  NE.lng = NE.lng ? (NE.lng > lng ? NE.lng : lng) : lng;
-	  // B.lat should be the lowest value
-	  SW.lat = SW.lat ? (SW.lat < lat ? SW.lat : lat) : lat;
-	  // B.lng should be the lowest value
-	  SW.lng = SW.lng ? (SW.lng < lng ? SW.lng : lng) : lng;
-	});
-  
-	return { NE: NE, SW: SW };
+export function getBounds(points, fct) {
+  const NE = { lat: 0, lng: 0 }; // Nord East
+  const SW = { lat: 0, lng: 0 }; // South West
+
+  points.forEach((item) => {
+    const lat = typeof fct === "function" ? fct(item)[0] : item[0]; // latitude
+    const lng = typeof fct === "function" ? fct(item)[1] : item[1]; // longitude
+
+    // NW.lat should be the highest value
+    NE.lat = NE.lat ? (NE.lat > lat ? NE.lat : lat) : lat;
+    // NW.lng should be the higgest value
+    NE.lng = NE.lng ? (NE.lng > lng ? NE.lng : lng) : lng;
+    // B.lat should be the lowest value
+    SW.lat = SW.lat ? (SW.lat < lat ? SW.lat : lat) : lat;
+    // B.lng should be the lowest value
+    SW.lng = SW.lng ? (SW.lng < lng ? SW.lng : lng) : lng;
+  });
+
+  return { NE: NE, SW: SW };
+}
+
+// MAP ZOOM LEVEL
+export function getZoomLevel(bounds, sizes) {
+  const WORLD_DIM = { height: 256, width: 256 };
+  const ZOOM_MAX = 21;
+
+  function latRad(lat) {
+    const sin = Math.sin((lat * Math.PI) / 180);
+    const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+    return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
   }
-  
-  // MAP ZOOM LEVEL
-  export function getZoomLevel(bounds, sizes) {
-	const WORLD_DIM = { height: 256, width: 256 };
-	const ZOOM_MAX = 21;
-  
-	function latRad(lat) {
-	  const sin = Math.sin((lat * Math.PI) / 180);
-	  const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
-	  return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
-	}
-  
-	function zoom(mapPx, worldPx, fraction) {
-	  return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
-	}
-  
-	const ne = bounds.NE;
-	const sw = bounds.SW;
-  
-	const latFraction = (latRad(ne.lat) - latRad(sw.lat)) / Math.PI;
-  
-	const lngDiff = ne.lng - sw.lng;
-	const lngFraction = (lngDiff < 0 ? lngDiff + 360 : lngDiff) / 360;
-  
-	const latZoom = zoom(sizes.height, WORLD_DIM.height, latFraction);
-	const lngZoom = zoom(sizes.width, WORLD_DIM.width, lngFraction);
-  
-	return Math.min(latZoom, lngZoom, ZOOM_MAX);
+
+  function zoom(mapPx, worldPx, fraction) {
+    return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
   }
-  
-  // MAP CENTER
-  export function getCenter(bounds) {
-	return [
-	  (bounds.NE.lat + bounds.SW.lat) / 2,
-	  (bounds.NE.lng + bounds.SW.lng) / 2,
-	];
-  }
-  
+
+  const ne = bounds.NE;
+  const sw = bounds.SW;
+
+  const latFraction = (latRad(ne.lat) - latRad(sw.lat)) / Math.PI;
+
+  const lngDiff = ne.lng - sw.lng;
+  const lngFraction = (lngDiff < 0 ? lngDiff + 360 : lngDiff) / 360;
+
+  const latZoom = zoom(sizes.height, WORLD_DIM.height, latFraction);
+  const lngZoom = zoom(sizes.width, WORLD_DIM.width, lngFraction);
+
+  return Math.min(latZoom, lngZoom, ZOOM_MAX);
+}
+
+// MAP CENTER
+export function getCenter(bounds) {
+  return [
+    (bounds.NE.lat + bounds.SW.lat) / 2,
+    (bounds.NE.lng + bounds.SW.lng) / 2,
+  ];
+}
