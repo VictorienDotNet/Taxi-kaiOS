2571196380f13c05e94e4fadb4462bbb325bb9a7
Victorien <bvictorien+github@gmail.com>
2023-03-25 23:34:46 +0100

    Debug process and corss error

--- src/tools/analytics.js
+++ src/tools/analytics.js
@@ -2,23 +2,23 @@ import amplitude from "amplitude-js";
 const version = process.env.REACT_APP_V;
 
 export const hit = (name, properties, id) => {
-	if (!name) return false;
+  if (!name) return false;
 
-	if (process.env.NODE_ENV !== "production") {
-		console.log(name, {
-			version: version,
-			...properties
-		});
-	}
+  if (process.env.NODE_ENV !== "production") {
+    console.log(name, {
+      version: version,
+      ...properties,
+    });
+  } else {
+    if (id) {
+      amplitude.getInstance().init(process.env.REACT_APP_STATS_APIKEY, id);
+    } else {
+      amplitude.getInstance().init(process.env.REACT_APP_STATS_APIKEY);
+    }
 
-	if (id) {
-		amplitude.getInstance().init(process.env.REACT_APP_STATS_APIKEY, id);
-	} else {
-		amplitude.getInstance().init(process.env.REACT_APP_STATS_APIKEY);
-	}
-
-	amplitude.getInstance().logEvent(name, {
-		version: version,
-		...properties
-	});
+    amplitude.getInstance().logEvent(name, {
+      version: version,
+      ...properties,
+    });
+  }
 };
